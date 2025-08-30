6a0b8ef26af166f83467d4e4c9c0d3b6aa2ba3de
Abrar <abrarchhapavala05@gmail.com>
2025-08-30 21:25:35 +0530

    resource schema created

---
+++ server/models/resource.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const resourceSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['ppt', 'notes', 'book', 'lab manual', 'mid paper', 'gtu paper'],
+        required: true
+    },
+    branch: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    file_url: {
+        type: String,
+        required: true
+    },
+    uploaded_by: {
+        type: mongoose.Schema.Types.Number,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending'
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Resource = mongoose.model('Resource', resourceSchema);
+
+module.exports = Resource;
