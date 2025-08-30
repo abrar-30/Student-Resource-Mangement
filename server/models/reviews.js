02cbef520e68052eae283de38a585b4c7733b34c
Abrar <abrarchhapavala05@gmail.com>
2025-08-30 21:29:06 +0530

    review schema created

---
+++ server/models/reviews.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    resource_id: {
+        type: mongoose.Schema.Types.Number,
+        ref: 'Resource',
+        required: true
+    },
+    user_id: {
+        type: mongoose.Schema.Types.Number,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
