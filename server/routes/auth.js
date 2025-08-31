a0b012a08e3ebc213086fdd6044f396017df1ca4
Abrar <abrarchhapavala05@gmail.com>
2025-08-31 13:20:33 +0530

    edited

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { authRegister } = require('../controller/auth');
+const { userRegister } = require('../controller/auth');
 const authRouter=express.Router();
 //hello from jay12
 authRouter.post('/register',userRegister);
