c6297916c569b979858e966097cc81d8a234a30c
Chhatrotiya <makwanajay925@gmail.com>
2025-08-31 13:07:00 +0530

    helllo from jay

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const { userRegister } = require('../controller/auth');
 const authRouter=express.Router();
-
-authRouter.post('/register',authRegister);
+//hello from jay
+authRouter.post('/register',userRegister);
 
 module.exports=authRouter;
