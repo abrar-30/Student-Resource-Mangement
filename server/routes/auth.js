96742a24e5bc4b4ac997f1d9d6c20aacbaad290d
Chhatrotiya <makwanajay925@gmail.com>
2025-08-31 13:33:26 +0530

    auth test

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const { userRegister } = require('../controller/auth');
 const authRouter=express.Router();
-
+//auth test
 authRouter.post('/register',userRegister);
 
 module.exports=authRouter;
