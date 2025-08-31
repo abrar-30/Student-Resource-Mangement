9db9a7a3999a03e4b9a1f6bd8ecee526eb707ae1
Abrar <abrarchhapavala05@gmail.com>
2025-08-31 12:03:56 +0530

    passprt error resolved

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
-const User = require('./models/users');
 const connect = require('./DB/connect');
-const jwt = require('jsonwebtoken');
-const passport = require('./config/passport');
-const user = require('./routes/user');
 require('dotenv').config();
+const passport = require('./config/passport'); // already configured passport
+const userRoutes = require('./routes/user');
 
-app.use(express.json()); // Parse JSON request bodies
-app.use('/api/user', user);
+// Middlewares
+app.use(express.json());
+app.use(passport.initialize());
+
+// Routes
+app.use('/api/user', userRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-// Initialize passport-local-mongoose strategy
-passport.use(User.createStrategy());
-
+// Connect DB and start server
 connect();
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port ' + (process.env.PORT || 3000));
 });
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,26 +1,27 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const User = require('../models/users');
 const passport = require('passport');
 require('dotenv').config();
+
 const opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
 };
 
 passport.use(
-    new JwtStrategy(opts, async (jwt_payload, done) => {
-        try {
-            const user = await User.findOne({ id: jwt_payload.id });
-            if (user) {
-                return done(null, user);
-            } else {
-                return done(null, false);
-            }
-        } catch (err) {
-            return done(err, false);
-        }
-    })
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      // Use _id instead of id
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
 );
 
 module.exports = passport;
