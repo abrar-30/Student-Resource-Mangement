06f3622b51d23c5ea6ae0a5a8080908291c34517
Chhatrotiya <makwanajay925@gmail.com>
2025-08-31 12:52:20 +0530

    auth changes

--- server/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const { userRegister } = require('../controller/user');
-const userRouter=express.Router();
-
-userRouter.post('/register',userRegister);
-
-module.exports=userRouter;
