c7e3be183e8180754303ca3e4ad1dff59e72b9e4
Chhatrotiya <makwanajay925@gmail.com>
2025-08-31 13:04:10 +0530

    auth change

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { userRegister } = require('../controller/auth');
+const { authRegister } = require('../controller/auth');
 const authRouter=express.Router();
 
 authRouter.post('/register',authRegister);
